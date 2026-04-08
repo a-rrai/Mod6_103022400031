c057dc8d20c6e73eb70085d97fb68dfff9952f81
a-rrai <raissha.njw@gmail.com>
2026-04-08 07:56:35 +0700

    Menambahkan dua class SayaMusicUser dan SayaMusicTrack

--- Modul6_103022400031/Program.cs
+++ Modul6_103022400031/Program.cs
@@ -1,2 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Modul6_103022400031
+{
+    class SayaMusicUser
+    {
+        private int id;
+        public string username;
+        private List<SayaMusicTrack> uploadedTracks;
+
+        public SayaMusicUser(string username)
+        {
+            this.username = username;
+
+            Random rndm = new Random();
+            this.id = rndm.Next(10000, 10000);
+
+            this.uploadedTracks = 0;
+        }
+
+        public int GetTotalPlayCount()
+        {
+            int total = 0;
+            foreach (var track in uploadedTracks)
+            {
+                total += track  ;
+            }
+            return total;
+        }
+
+        public void AddTrack(SayaMusicTrack track)
+        {
+            uploadedTracks.Add(track);
+        }
+
+        public void PrintAllTracks()
+        {
+            Console.WriteLine("User: " + username);
+        }
+    }
+
+    class SayaMusicTrack
+    {
+        private int id;
+        public string title;
+        private int playCount;
+
+        public SayaMusicTrack(string title)
+        {
+            this.title = title;
+
+            Random rndm = new Random();
+            this.id = rndm.Next(10000, 10000);
+
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("Track 1 judul: " + 100);
+            Console.WriteLine("Track 2 judul: " + 150);
+        }
+    }
+
+    class Program
+    {
+
+    }
+}
